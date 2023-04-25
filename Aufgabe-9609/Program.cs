2c008b087d8f0f222db1eaf0cf569b1a36b1bce8
amir.gilani <amir.gilani@CPHHEAD003>
2023-04-25 16:14:51 +0200

    Add project files.

---
+++ Aufgabe-9609/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Schreiben Sie eine Methode „isSquare“ mit Rückgabetyp bool und Parametertyp Integer. 
+ * Diese Methode soll herausfinden ob die übergebene Zahl eine Quadratzahl ist, 
+ * wenn dies der Fall ist soll diese true zurückgeben und falls nicht false.
+
+Beispiele
+isSquare(-1) =>  false
+isSquare(0) =>   true
+isSquare(3) =>   false
+isSquare(4) =>   true
+isSquare(25) =>  true 
+isSquare(26) =>  false
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_9609
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static IsSuqre()
+    }
+}
